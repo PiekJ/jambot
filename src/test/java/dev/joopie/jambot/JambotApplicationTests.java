591984913e945597da8d2922df05938e751bacd4
PiekJ <jos.piek@kpnmail.nl>
2021-09-19 16:21:24 +0200

    Remove application context test until jda mock.

--- src/test/java/dev/joopie/jambot/JambotApplicationTests.java
+++ src/test/java/dev/joopie/jambot/JambotApplicationTests.java
@@ -3,10 +3,10 @@ package dev.joopie.jambot;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class JambotApplicationTests {
 
-	@Test
+//	@Test
 	void contextLoads() {
 	}
 
