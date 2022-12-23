22cc60ec1b9bfdc5e73bfecb94fd6d5c78c5b099
Carsten Flokstra <github@carstenflokstra.com>
2022-12-23 23:59:30 +0100

    Add SpotifyAPI to search for tracks and playlist via /play command (#11)

---
+++ src/main/java/dev/joopie/jambot/api/spotify/ApiSpotifyService.java
@@ -0,0 +1,111 @@
+package dev.joopie.jambot.api.spotify;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.hc.core5.http.ParseException;
+import org.springframework.stereotype.Service;
+import se.michaelthelin.spotify.SpotifyApi;
+import se.michaelthelin.spotify.exceptions.SpotifyWebApiException;
+import se.michaelthelin.spotify.model_objects.credentials.ClientCredentials;
+import se.michaelthelin.spotify.model_objects.specification.*;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class ApiSpotifyService {
+    private static final Pattern SPOTIFY_URL_PATTERN = Pattern.compile("https?:\\/\\/(?:open\\.)?spotify.com\\/(user|episode|playlist|track)\\/(?:spotify\\/playlist\\/)?(\\w*)");
+    private final SpotifyProperties properties;
+    private SpotifyApi spotifyApi;
+    private ClientCredentials clientCredentials;
+
+    public void initSpotifyAccessToken() {
+        spotifyApi = new SpotifyApi.Builder()
+                .setClientId(properties.getClientId())
+                .setClientSecret(properties.getClientSecret())
+                .build();
+
+        final var clientCredentialsRequest = spotifyApi.clientCredentials()
+                .build();
+
+        try {
+            clientCredentials = clientCredentialsRequest.execute();
+            spotifyApi.setAccessToken(clientCredentials.getAccessToken());
+
+            log.debug("Expires in: {}", clientCredentials.getExpiresIn());
+        } catch (IOException | SpotifyWebApiException | ParseException e) {
+            log.error(e.getMessage(), e);
+        }
+    }
+
+    public Optional<String> getTrack(String link) {
+        if (spotifyApi == null || spotifyApi.getAccessToken().isEmpty() || clientCredentials.getExpiresIn() < 0) {
+            initSpotifyAccessToken();
+        }
+        final var trackId = getSpotifyIdFromLink(link);
+
+        if (trackId.isEmpty()) {
+            return Optional.empty();
+        }
+        final var getTrackRequest = spotifyApi.getTrack(trackId.get()).build();
+
+        try {
+            final var track = getTrackRequest.execute();
+            return Optional.of(getFormattedTrack(track));
+
+        } catch (IOException | SpotifyWebApiException | ParseException e) {
+            log.error(e.getMessage(), e);
+        }
+
+        return Optional.empty();
+    }
+
+    public List<String> getTracksFromPlaylist(String link) {
+        if (spotifyApi == null || spotifyApi.getAccessToken().isEmpty() || clientCredentials.getExpiresIn() < 0) {
+            initSpotifyAccessToken();
+        }
+        final var playlistId = getSpotifyIdFromLink(link);
+
+        if (playlistId.isEmpty()) {
+            return List.of();
+        }
+        final var playlistsItemsRequest = spotifyApi.getPlaylistsItems(playlistId.get()).build();
+
+        try {
+            final var playlist = playlistsItemsRequest.execute();
+            return Arrays.stream(playlist.getItems())
+                    .map(playlistItem -> (Track) playlistItem.getTrack())
+                    .map(this::getFormattedTrack)
+                    .toList();
+
+        } catch (IOException | SpotifyWebApiException | ParseException e) {
+            log.error(e.getMessage(), e);
+            return List.of();
+        }
+    }
+
+    private String getFormattedTrack(Track track) {
+        var artists = Arrays.stream(track.getArtists())
+                .map(ArtistSimplified::getName)
+                .collect(Collectors.joining(","));
+        return "%s-%s".formatted(artists, track.getName());
+    }
+
+    private Optional<String> getSpotifyIdFromLink(String link) {
+        final var matcher = SPOTIFY_URL_PATTERN.matcher(link);
+
+        if (matcher.find()) {
+            return Optional.of(matcher.group(2));
+        } else {
+            log.debug("No ID found in the link.");
+            return Optional.empty();
+        }
+    }
+}
--- src/main/java/dev/joopie/jambot/music/command/PlayCommandHandler.java
+++ src/main/java/dev/joopie/jambot/music/command/PlayCommandHandler.java
@@ -1,5 +1,6 @@
 package dev.joopie.jambot.music.command;
 
+import dev.joopie.jambot.api.spotify.ApiSpotifyService;
 import dev.joopie.jambot.api.youtube.ApiYouTubeService;
 import dev.joopie.jambot.api.youtube.SearchResultDto;
 import dev.joopie.jambot.command.CommandHandler;
@@ -8,7 +9,9 @@ import dev.joopie.jambot.music.JambotMusicServiceException;
 import dev.joopie.jambot.api.youtube.JambotYouTubeException;
 import dev.joopie.jambot.music.GuildMusicService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.interactions.commands.Command;
 import net.dv8tion.jda.api.interactions.commands.CommandInteraction;
@@ -17,22 +20,30 @@ import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.requests.RestAction;
+import net.dv8tion.jda.api.requests.restaction.WebhookMessageCreateAction;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.springframework.stereotype.Component;
 
 import java.awt.*;
-import java.util.Objects;
+import java.util.*;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 @Component
+@Slf4j
 @RequiredArgsConstructor
 public class PlayCommandHandler implements CommandHandler {
     private static final String COMMAND_NAME = "jam";
     private static final String COMMAND_OPTION_INPUT_NAME = "input";
-
+    private static final String SPOTIFY_URL = "spotify";
     private static final Pattern URL_OR_ID_PATTERN = Pattern.compile("^(http(|s)://.*|[\\w\\-]{11})$");
-
     private final GuildMusicService musicService;
     private final ApiYouTubeService apiYouTubeService;
+    private final ApiSpotifyService apiSpotifyService;
+    private final boolean isAvailable = false;
 
     @Override
     public Command.Type type() {
@@ -45,7 +56,7 @@ public class PlayCommandHandler implements CommandHandler {
                 .addOption(
                         OptionType.STRING,
                         COMMAND_OPTION_INPUT_NAME,
-                        "YouTube, Soundcloud etc url or search term",
+                        "YouTube, Soundcloud, Spotify url or search term",
                         true);
     }
 
@@ -68,28 +79,76 @@ public class PlayCommandHandler implements CommandHandler {
         try {
             final var input = inputOption.getAsString();
 
-            if (URL_OR_ID_PATTERN.matcher(input).matches()) {
-                musicService.play(event.getGuild(), event.getUser(), input);
-
-                // TODO: unify responses for both url/search with embed which song got queued.
-                return event.getHook()
-                        .sendMessage("OK, we added the track to the queue! %s".formatted(input));
-            } else {
-                final var dto = apiYouTubeService.searchForSong(input);
-                if (dto.isFound()) {
-                    musicService.play(event.getGuild(), event.getUser(), dto.getVideoId());
-
-                    return event.getHook()
-                            .sendMessageEmbeds(createMessageEmbedOfSearchTrack(event.getGuild().getName(), dto));
-                }
+            if (input.contains(SPOTIFY_URL)) {
+                return handleSpotifyLink(event, input);
             }
+
+            if (URL_OR_ID_PATTERN.matcher(input).matches()) {
+                return handleNormalLink(event, input);
+            } else {
+                return handleYoutubeSearch(event, input);
+            }
+
         } catch (JambotMusicServiceException | JambotMusicPlayerException | JambotYouTubeException exception) {
             return event.getHook().sendMessage(exception.getMessage())
                     .setEphemeral(true);
         }
+    }
 
+    private WebhookMessageCreateAction<Message> handleYoutubeSearch(CommandInteraction event, String input) {
+        final var dto = apiYouTubeService.searchForSong(input);
+        if (dto.isFound()) {
+            musicService.play(event.getGuild(), event.getUser(), dto.getVideoId());
+
+            return event.getHook()
+                    .sendMessageEmbeds(createMessageEmbedOfSearchTrack(event.getGuild().getName(), dto));
+        }
         return event.getHook()
-                .sendMessage("Well... we tried, but were unable to find a track.")
+                .sendMessage("Or the internet is broken, or we did not have Dora the Explorer to help to find the track that you are looking for. No results found for the given search result")
+                .setEphemeral(true);
+    }
+
+
+    private WebhookMessageCreateAction<Message> handleNormalLink(CommandInteraction event, String input) {
+        musicService.play(event.getGuild(), event.getUser(), input);
+        return event.getHook()
+                .sendMessage("OK, we added the track to the queue! %s".formatted(input));
+    }
+
+
+    private WebhookMessageCreateAction<Message> handleSpotifyLink(CommandInteraction event, String input) {
+        if (input.contains("track")) {
+            final var spotifyResult = apiSpotifyService.getTrack(input);
+            if (spotifyResult.isEmpty()) {
+                return event.getHook()
+                        .sendMessage("That's bad! We could not find anything for the given Spotify link. Did you copy the correct link?")
+                        .setEphemeral(true);
+            }
+            return handleYoutubeSearch(event, spotifyResult.get());
+
+        } else if (input.contains("playlist")) {
+            if (isAvailable) {
+                final var counter = new AtomicInteger(0);
+                apiSpotifyService.getTracksFromPlaylist(input)
+                        .forEach(result -> {
+                            var playlistresult = apiYouTubeService.searchForSong(result);
+
+                            if (playlistresult.isFound()) {
+                                musicService.play(event.getGuild(), event.getUser(), playlistresult.getVideoId());
+                                counter.getAndIncrement();
+                            }
+                        });
+
+                return event.getHook()
+                        .sendMessage("Imported playlist. Queued %s items!".formatted(counter));
+            } else {
+                return event.getHook()
+                        .sendMessage("The use of Spotify Playlist links is currently not available")
+                        .setEphemeral(true);
+            }
+        }
+        return event.getHook()
+                .sendMessage("Well this is akward. But I do not know what to do with this Spotify Link")
                 .setEphemeral(true);
     }
 
